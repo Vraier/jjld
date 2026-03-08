8ef4a162227769e55a8ad43308ffda0ee9a28293
Julia Himmel <2065352+TwoFX@users.noreply.github.com>
2026-03-08 15:07:31 +0100

    Rename example.cpp -> example.c

--- example/compile.sh
+++ example/compile.sh
@@ -1,2 +1,2 @@
-gcc -static -static-libgcc -static-libstdc++ -c -o example.o example.cpp
+gcc -static -static-libgcc -static-libstdc++ -c -o example.o example.c
 gcc -static -static-libgcc -static-libstdc++ -o example example.o
