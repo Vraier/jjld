2381eba54d513172be6e46fd23bce112c6c7e508
Vraier <jpvdheydt@googlemail.com>
2026-03-08 14:03:29 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Analyze obj Files
+- gcc: `-c` to only compile and not link
+- objdump: `-d` to find assembly instructions
+- readelf: `-a` to print everything
+
+# Useful Material
+https://inai.de/documents/Linkers.pdf
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "C++ development environment for linker project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            gcc
+            binutils
+            gnumake
+            cmake
+            gdb
+          ];
+        };
+      }
+    );
+}
