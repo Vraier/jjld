51650918e6bf8735e27679d8f4d8e901740cb2e2
Julia Himmel <2065352+TwoFX@users.noreply.github.com>
2026-03-08 16:28:36 +0100

    no unwind tables

--- example/compile.sh
+++ example/compile.sh
@@ -1,8 +1,8 @@
 # Compile the print library into an object file
-gcc -c -nostdlib -fno-builtin my_print.c -o my_print.o
+gcc -c -nostdlib -fno-builtin -fno-unwind-tables -fno-asynchronous-unwind-tables my_print.c -o my_print.o
 
 # Compile main into an object file
-gcc -c -nostdlib -fno-builtin my_main.c -o my_main.o
+gcc -c -nostdlib -fno-builtin -fno-unwind-tables -fno-asynchronous-unwind-tables my_main.c -o my_main.o
 
 # Link the two object files together into the final executable
 ld my_main.o my_print.o -o my_main
